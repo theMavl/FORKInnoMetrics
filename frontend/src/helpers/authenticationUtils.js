c936cc273dcbd2c0f2df9ab4729507a727e2c184
Mavl <bmcmavl@gmail.com>
2019-05-04 12:48:30 +0300

    Hashing password on registering and login

--- frontend/src/helpers/authenticationUtils.js
+++ frontend/src/helpers/authenticationUtils.js
@@ -1,29 +1,38 @@
-import { history } from './history'
+import {history} from './history'
 import _ from 'lodash'
+var pbkdf2 = require('pbkdf2');
 
 export const isTokenExists = () => {
-  return null != localStorage.getItem('user')
-      && null != JSON.parse(localStorage.getItem('user')).token
+    return null != localStorage.getItem('user')
+        && null != JSON.parse(localStorage.getItem('user')).token
 }
 
 export const getToken = () => {
-  if(isTokenExists()){
-    return JSON.parse(localStorage.getItem('user')).token
-  }
-  return ''
+    if (isTokenExists()) {
+        return JSON.parse(localStorage.getItem('user')).token
+    }
+    return ''
 }
 
 export const saveUserToLocalStorage = (token) => {
-  localStorage.setItem('user', JSON.stringify({token: token}))
+    localStorage.setItem('user', JSON.stringify({token: token}))
 }
 
 export const removeUserFromLocalStorage = () => {
-  localStorage.removeItem('user')
+    localStorage.removeItem('user')
 }
 
 export const redirectFromAuth = () => {
-  let nextLocation = _.get(history, 'location.state.from', '/dashboard')
-  if(nextLocation === '/login' || nextLocation === '/register')
-    nextLocation = '/dashboard'
-  history.push(nextLocation)
+    let nextLocation = _.get(history, 'location.state.from', '/dashboard')
+    if (nextLocation === '/login' || nextLocation === '/register')
+        nextLocation = '/dashboard'
+    history.push(nextLocation)
+}
+
+export const generatePasswordHash = (login, password) => {
+    pbkdf2.pbkdf2(password, login, 100000, 128, 'sha512', (err, derivedKey) => {
+        if (err) throw err;
+        // console.log(derivedKey.toString('hex'));
+        return derivedKey.toString('hex');
+    });
 }
